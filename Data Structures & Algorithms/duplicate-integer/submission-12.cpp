e0410fbb6c567382fcafd524144b556228384e1a
V1JAYP <142262043+V1JAYP@users.noreply.github.com>
2026-09-02 11:45:42 -0500

    Add: duplicate-integer - submission-12

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> set1(nums.begin(), nums.end());
+        if(nums.size() != set1.size() ){
+            return true;
+        }
+        return false;
+        
+    }
+};
