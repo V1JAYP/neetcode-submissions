7eddb1a8f96b2d91dd6ba7790f584703c89d7a57
V1JAYP <142262043+V1JAYP@users.noreply.github.com>
2026-09-03 17:30:33 -0500

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map1;
+        int n = nums.size();
+
+        for(int i = 0; i < n ; i++ ){
+            int complement = target - nums[i];
+            if(map1.count(complement)){
+                return {map1[complement], i };
+            }
+            map1[nums[i]] = i;
+        }
+
+        return {};
+
+        
+    }
+};
